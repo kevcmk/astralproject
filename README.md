7f9f6fd5d668823bb30c6b99fee7530d228802c1
Kevin Katz <kevcmk@gmail.com>
2019-09-22 20:22:55 -0700

    Injected.

---
+++ README.md
@@ -0,0 +1,15 @@
+# cdpd
+
+# Notes
+https://developer.chrome.com/extensions/getstarted
+
+
+# Terms of Service
+Relevant clauses of Coderpad's Terms of Service
+
+Subscriber shall:
+...
+9. not permit direct or indirect access to or use the Service in a way that circumvents a usage or capacity limit of the Service;
+...
+13. not reverse engineer, decompile, translate, disassemble or otherwise attempt to extract any or all of the source code of the Service, except as permitted by applicable laws or governmental regulations;
+
--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 var io = require('socket.io')(80);
 var fs = require('fs');
 
+const configFilename = 'file.txt'
 io.sockets.on('connection', function (socket) {
-    socket.emit('server', {message: 'hello'});
+    console.log(`Connected: ${socket.id}`)
+    emitFile(configFilename)
 });
 
 // the code below will check for change every 250ms and emit a message to all clients of / , and inform that the file has changed
-fs.watchFile('file.txt', {persistent: true, interval: 250}, function (curr, prev) {
-    console.log('File changed.')
-    io.emit('server', {message: JSON.stringify(curr)});
+fs.watch(configFilename, {encoding: 'buffer'}, function (eventType, filename) {
+
+    if (eventType === "change") {
+        console.log(`${filename} changed at ${new Date().toTimeString()}, reading...`)
+
+        emitFile(filename)
+
+
+    } else {
+        console.log(`Unhandled event, ${eventType}, skipping.`)
+        console.log(`Filename: ${filename}`)
+
+    }
+
 })
 
+function emitFile(filename) {
+    fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) throw err;
+        console.debug('Read.');
 
+        console.log(JSON.stringify(data).slice(0,40))
+        io.emit('server', {text: data, modification_time: new Date().toISOString()});
+    });
+}
